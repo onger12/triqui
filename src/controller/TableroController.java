a1d05d5843853f4168fff27c92f61aa7ba284fc1
Dujardin <dujardin.univalle@gmail.com>
2023-04-14 17:42:03 -0500

    Added tableroController

---
+++ src/controller/TableroController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TableroController implements ActionListener {
+
+  public TableroController() {
+    super();
+    
+  }
+
+  @Override
+  public void actionPerformed(ActionEvent e) {
+
+  }
+  
+}
